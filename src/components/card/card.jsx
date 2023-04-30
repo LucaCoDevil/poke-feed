50468e809a7523b896a6e0f84c47acb7faac3afb
LucaCoDevil <lucacodevilla123@gmail.com>
2023-04-30 11:55:16 +0200

    Card styling: image platform effect

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -4,17 +4,16 @@ export default function card(props) {
   return (
     <div className="col-4 mb-4">
       <div className="card">
-        <img src="..." className="card-img-top" alt="..." />
+        <div className="image-wrapper">
+          <img
+            className="card-image"
+            src={props.imageSource}
+            width="60"
+            height="60"
+          />
+        </div>
         <div className="card-body">
-          <div className="image-wrapper">
-            <img
-              className="card-image"
-              src={props.imageSource}
-              width="60"
-              height="60"
-            />
-          </div>
-          <h5 className="card-title">{props.pokemonName}</h5>
+          <h3 className="card-title">{props.pokemonName}</h3>
           {props.stats.map((stat, index) => (
             <p className="card-text" key={index}>
               {stat.stat.name}: {stat.base_stat} {stat.effort}
